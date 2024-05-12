6010b7aececa53f5833793eb3699cfa60da82d87
Milena Aphiner <161851079+Milena-Aphiner@users.noreply.github.com>
2024-05-12 20:05:06 -0300

    criação do cadastro e bd

--- projeto-integrador-I/main.py
+++ projeto-integrador-I/main.py
@@ -1,7 +1,32 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask_login import LoginManager, login_user, login_required, logout_user, UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from models import User
+import sqlite3
 
 app = Flask (__name__)
 app.config['SECRET_KEY'] = 'MILENAPHINER'
+app.config['DATABASE'] = 'my_database.db'  
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_database.db'
+
+
+def get_db_connection():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+db = SQLAlchemy(app)
+
+
+def create_user_table():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, nome TEXT NOT NULL, senha TEXT NOT NULL)')
+    conn.commit()
+    conn.close()
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def homepage():
@@ -28,13 +53,43 @@ def contatos():
     return render_template('contatos.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-
-    nome = request.form.get('nome')
-    senha = request.form.get('senha')
-
+    if request.method == 'POST':
+        nome = request.form['nome']
+        senha = request.form['senha']
+        user = User.query.filter_by(nome=nome).first()
+        if user and user.senha == senha:
+            login_user(user)
+            flash('Login bem-sucedido!', 'success')
+            return redirect(url_for('usuario'))
+        else:
+            flash('Credenciais inválidas. Tente novamente.', 'danger')
     return render_template('login.html')
+
+@app.route('/usuario')
+@login_required
+def usuario():
+    return f'Bem-vindo!'
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Logout realizado com sucesso.', 'success')
+    return redirect(url_for('homepage'))
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    if request.method == 'POST':
+        new_user = User(nome=request.form['nome'],
+                        senha=request.form['senha'])
+        print(request.form)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Usuário cadastrado com sucesso!', 'success')
+        return redirect(url_for('login'))
+    return render_template('cadastro.html')
 
 if __name__== "__main__":
     app.run(debug=True)
